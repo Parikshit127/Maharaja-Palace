f8a2cab8e07b03c7d88f32368336bd6025200440
Harleen3108 <151908029+Harleen3108@users.noreply.github.com>
2025-12-03 13:07:16 +0530

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "http://localhost:5174",
-      "https://maharaja-palace-nine.vercel.app/",
+      "https://maharaja-palace-nine.vercel.app",
     ],
     credentials: true,
   })
